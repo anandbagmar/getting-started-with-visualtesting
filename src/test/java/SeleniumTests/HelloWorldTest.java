57083148ebe8bb37d3e1e74886accc5719c9c275
Anand Bagmar <abagmar@gmail.com>
2023-12-09 08:35:38 +0530

    added assertions in non-eyes test

--- src/test/java/SeleniumTests/HelloWorldTest.java
+++ src/test/java/SeleniumTests/HelloWorldTest.java
@@ -2,11 +2,14 @@ package SeleniumTests;
 
 import Utilities.Driver;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import static Utilities.Wait.waitFor;
 
 public class HelloWorldTest {
 
@@ -23,14 +26,27 @@ public class HelloWorldTest {
     public void seleniumBaseTest() throws InterruptedException {
         driver.get("https://applitools.com/helloworld");
 
+        String actualDefaultNumber = driver.findElement(By.xpath("//span[@class='primary']")).getText();
+        System.out.println("actualDefaultNumber: " + actualDefaultNumber);
+        String expectedDefaultNumber = "123456";
+        Assertions.assertEquals(expectedDefaultNumber, actualDefaultNumber, "Default number is incorrect");
+        String randomNumber = expectedDefaultNumber;
         for (int stepNumber = 0; stepNumber < numOfSteps; stepNumber++) {
             driver.findElement(By.linkText("?diff1"))
                     .click();
-            Thread.sleep(1000);
+            String actualRandomNumber = driver.findElement(By.cssSelector("span.diff1.diff2.random-number")).getText();
+            System.out.println("actualRandomNumber: " + actualRandomNumber);
+            Assertions.assertNotEquals(randomNumber, actualRandomNumber, "Random number is same as earlier number");
+            waitFor(1);
+            randomNumber = actualRandomNumber;
         }
 
         driver.findElement(By.tagName("button"))
                 .click();
+        String actualSuccessfulMessage = driver.findElement(By.cssSelector("div.section.image-section")).findElement(By.cssSelector("p")).getText();
+        System.out.println("actualSuccessfulMessage: " + actualSuccessfulMessage);
+        String expectedSuccessfulMessage = "You successfully clicked the button!";
+        Assertions.assertEquals(expectedSuccessfulMessage, actualSuccessfulMessage, "Successful message is incorrect");
     }
 
     @AfterEach
