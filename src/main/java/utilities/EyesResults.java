00631ca47724be509852a03b8d99ebedc224ca7f
Anand Bagmar <abagmar@gmail.com>
2024-06-13 18:47:51 +0800

    updated EyesResults to include accessibility results

--- src/main/java/utilities/EyesResults.java
+++ src/main/java/utilities/EyesResults.java
@@ -1,5 +1,6 @@
 package utilities;
 
+import com.applitools.eyes.SessionAccessibilityStatus;
 import com.applitools.eyes.TestResults;
 
 public class EyesResults {
@@ -11,16 +12,22 @@ public class EyesResults {
         System.out.println(result);
         System.out.println("\tTest Name: " + result.getName() + " :: " + result);
         System.out.println("\tTest status: " + result.getStatus());
-        System.out.printf("\t\tName = '%s', %nBrowser = %s,OS = %s, viewport = %dx%d, matched = %d, mismatched = %d, missing = %d, aborted = %s%n",
-                result.getName(),
-                result.getHostApp(),
-                result.getHostOS(),
-                result.getHostDisplaySize().getWidth(),
-                result.getHostDisplaySize().getHeight(),
-                result.getMatches(),
-                result.getMismatches(),
-                result.getMissing(),
-                (result.isAborted() ? "aborted" : "no"));
+        SessionAccessibilityStatus accessibilityStatus = result.getAccessibilityStatus();
+        String accessibilityResults = (accessibilityStatus==null) ? "Accessibility not set" : "Accessibility validation: " + accessibilityStatus.getStatus().getName() + " at level: " + accessibilityStatus.getLevel().getName() + " with guideline version: " + accessibilityStatus.getVersion().getName();
+        System.out.printf("\t\tName = '%s', " +
+                          "%n\t\tBrowser = %s, OS = %s, Viewport = %dx%d, " +
+                          "%n\t\t%s, " +
+                          "%n\t\tmatched = %d, mismatched = %d, missing = %d, aborted = %s%n",
+                          result.getName(),
+                          result.getHostApp(),
+                          result.getHostOS(),
+                          result.getHostDisplaySize().getWidth(),
+                          result.getHostDisplaySize().getHeight(),
+                          accessibilityResults,
+                          result.getMatches(),
+                          result.getMismatches(),
+                          result.getMissing(),
+                          (result.isAborted() ? "aborted" : "no"));
         System.out.println("Results available here: " + result.getUrl());
         hasMismatches = result.getMismatches() != 0 || result.isAborted();
         System.out.println("Visual validation failed? - " + hasMismatches);
